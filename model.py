cbaace65dc72645a7f517898052682fd5d301984
joaocesarDev <mercadinhodoleo95@gmail.com>
2023-01-24 18:24:24 -0300

    model projeto mercearia

---
+++ model.py
@@ -0,0 +1,68 @@
+# modelagem dos dados
+
+from datetime import date
+
+
+class Categoria:
+
+    def __init__(self, categoria):
+        self.categoria = categoria
+
+
+class Produtos:
+
+    def __init__(self, nome, preco, categoria):
+        self.nome = nome
+        self.preco = preco
+        self.categoria = categoria
+
+
+class Estoque:
+
+    def __init__(self, produto, quantidade):
+        self.produto = produto
+        self.quantidade = quantidade
+
+
+class Vendas:
+
+    def __init__(self, itensVendidos: Produtos, vendedor, comprador, quantidadeVendida, datatime: date):
+        self.itensVendidos = itensVendidos
+        self.vendedor = vendedor
+        self.comprador = comprador
+        self.quantidadeVendida = quantidadeVendida
+        datatime = datatime
+
+
+class Fornecedor:
+
+    def __init__(self, nome, telefone, cnpj, categoria):
+        self.nome = nome
+        self.telefone = telefone
+        self.cnpj = cnpj
+        self.categoria = categoria
+
+    
+
+class Pessoas:
+
+    def __init__(self, nome, cpf, email, telefone, endereco):
+        self. nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.telefone = telefone
+        self.endereco = endereco
+
+
+class Cliente:
+
+    def __init__(self, clt, nome, cpf, email, telefone, endereco):
+        self.clt = clt
+        self.nome = nome
+        self.cpf = cpf
+        self.email = email
+        self.telefone = telefone
+        self.endereco = endereco
+
+
+
