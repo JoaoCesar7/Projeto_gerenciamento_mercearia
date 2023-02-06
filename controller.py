7a11e80806d2e1b192358ca757b037c4e90ebe1b
joaocesarDev <mercadinhodoleo95@gmail.com>
2023-02-05 22:27:01 -0300

    atualizando removerProduto

--- controller.py
+++ controller.py
@@ -24,15 +24,13 @@ class CategoriaController:
         else:
             print('A categoria já existe em nossa base de dados.')
 
-
-
     @classmethod
     def removerCategoria(cls, removerCategoria):  # Remover Categoria
         x = CategoriaDao.ler_categoria()
         print('')
         cate = list(filter(lambda x: x.categoria.replace('\n', '') == removerCategoria, x))
         if len(cate) == 0:
-            print(f'Categoria NÃO existe em nossa base de dados.')
+            print('Categoria NÃO existe em nossa base de dados.')
         else:
             for i in range(len(x)):
                 if x[i].categoria.replace('\n', '') == removerCategoria:
@@ -44,8 +42,6 @@ class CategoriaController:
             with open('categoria.txt', 'w') as arq:
                 for i in x:
                     arq.writelines(i.categoria)
-
-
     
     @classmethod
     def alterarCategoria(cls, alterarCategoria, alteradaCategoria):  # Alterar Categoria
@@ -73,7 +69,6 @@ class CategoriaController:
                 print('Categoria alterada com sucesso!')
 
 
-       
 
 # CLASSES DA ÁREA DE PRODUTOS
 
@@ -93,29 +88,35 @@ class ProdutosController:
         else:
             print('Produto existe em nossa base de dados.')
 
-
-
     @classmethod
     def removerProduto(cls, nomeProduto):
         x = ProdutosDao.ler()
+        print('')
 
-        produ = list(map(lambda x: x == nomeProduto, x))
-        if len(produ) == 0:
+        produ = list(filter(lambda x: x.nome.replace('\n', '') == nomeProduto, x))
+        if produ == 0:
             print('Não existe')
         else:
-            for i in range(0, len(x)):
-                print(i)
-                if i.nome == nomeProduto:
-                    del i
+            for i in range(len(x)):
+                if x[i].nome.replace('\n', '') == nomeProduto:
+                    del x[i]
                     break
-                    
-
 
 
             with open('produtos.txt', 'w') as arq:
                 for i in x:
-
                     arq.writelines(i.nome)
+
+    @classmethod
+    def alterarProduto(cls, alterarProduto, nome, preco, categoria):
+        x = ProdutosDao.ler()
+
+        pro = list(filter(lambda x: x.nome == alterarProduto, x))
+        if len(pro) == 0:
+            print('Produto NÃO existe em nossa base de dados.')
+        else:
+            for i in range(0, len(x)):
+                print(i)
 
 
 
@@ -123,7 +124,18 @@ class ProdutosController:
 
 class Estoque:
     @classmethod
-    def cadastrarEstoque(cls, nome, produto):
-        ...
+    def cadastrarEstoque(cls, nome, preco, categoria, quantidade):
+        existe = False
+        x = EstoqueDao.salvar()
+        est = list(filter(lambda x: x == nome == preco == categoria == quantidade))
 
+
+
+
+
+class Fornecedor:
+
+    @classmethod
+    def cadastrar(cls, nome, telefone, cnpj, categoria):
+        ...
 
