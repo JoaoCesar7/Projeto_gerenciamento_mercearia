f39d301abcad60c2422bb1f6f3a239d2c4917495
joaocesarDev <mercadinhodoleo95@gmail.com>
2023-02-22 22:03:34 -0300

    Classe funcionario recebendo herança

--- model.py
+++ model.py
@@ -1,15 +1,18 @@
 # modelagem dos dados
 
+
 from datetime import date
 
 
 class Categoria:
+
 
     def __init__(self, categoria):
         self.categoria = categoria
 
 
 class Produtos:
+
 
     def __init__(self, nome, preco, categoria):
         self.nome = nome
@@ -19,12 +22,14 @@ class Produtos:
 
 class Estoque:
 
+
     def __init__(self, produtos: Produtos, quantidade):
         self.produtos = produtos
         self.quantidade = quantidade
 
 
 class Vendas:
+
 
     def __init__(self, itensVendidos: Produtos, vendedor, comprador, quantidadeVendida, datatime: date):
         self.itensVendidos = itensVendidos
@@ -36,25 +41,28 @@ class Vendas:
 
 class Fornecedor:
 
+
     def __init__(self, nome, telefone, cnpj, categoria):
         self.nome = nome
         self.telefone = telefone
         self.cnpj = cnpj
         self.categoria = categoria
 
-    
 
 class Cliente:
 
+
     def __init__(self, nome, cpf, email, telefone, endereco):
-        self. nome = nome
+        self.nome = nome
         self.cpf = cpf
         self.email = email
         self.telefone = telefone
         self.endereco = endereco
 
 
-class Funcionario:
+
+class Funcionario(Cliente):
+
 
     def __init__(self, clt, nome, cpf, email, telefone, endereco, numeroClt, serieClt, emissaoClt):
         self.clt = clt
@@ -62,5 +70,6 @@ class Funcionario:
         self.serieClt = serieClt
         self.emissaoClt = emissaoClt
         super(Funcionario, self).__init__(nome, cpf, email, telefone, endereco)
+
 
 
