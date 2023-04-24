99729e393c6fa57c4493ba48cf236911748d1581
joaocesarz <mercadinhodoleo95@gmail.com>
2023-04-24 17:47:31 -0300

    Pep 8

--- model.py
+++ model.py
@@ -4,29 +4,32 @@ from datetime import datetime
 
 
 class Categoria:
-
     def __init__(self, categoria):
         self.categoria = categoria
 
 
 class Produtos:
-
     def __init__(self, nome, preco, categoria):
         self.nome = nome
         self.preco = preco
-        self.categoria = categoria 
+        self.categoria = categoria
 
 
 class Estoque:
-
     def __init__(self, produto: Produtos, quantidade):
         self.produto = produto
         self.quantidade = quantidade
 
 
 class Vendas:
-
-    def __init__(self, itens_vendidos: Produtos, vendedor, comprador, quantidade_vendida, data = datetime.now().strftime("%d/%m/%Y")):
+    def __init__(
+        self,
+        itens_vendidos: Produtos,
+        vendedor,
+        comprador,
+        quantidade_vendida,
+        data=datetime.now().strftime('%d/%m/%Y'),
+    ):
         self.itens_vendidos = itens_vendidos
         self.vendedor = vendedor
         self.comprador = comprador
@@ -35,7 +38,6 @@ class Vendas:
 
 
 class Fornecedor:
-
     def __init__(self, nome, telefone, cnpj, categoria):
         self.nome = nome
         self.telefone = telefone
@@ -44,7 +46,6 @@ class Fornecedor:
 
 
 class Cliente:
-
     def __init__(self, nome, cpf, email, telefone, endereco):
         self.nome = nome
         self.cpf = cpf
@@ -54,7 +55,6 @@ class Cliente:
 
 
 class Funcionario(Cliente):
-
     def __init__(self, nome, cpf, email, telefone, endereco, clt):
         self.clt = clt
         super(Funcionario, self).__init__(nome, cpf, email, telefone, endereco)
